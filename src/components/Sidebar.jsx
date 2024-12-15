82534d28d2599ca40d8dd3f1f442123481e94e2f
mayank151c <mc76974@gmail.com>
2024-12-15 23:15:03 +0530

    Integrate MUI and Homepage fixed for Mobile screen size

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import '../styles/Sidebar.css';
 import SideItem from './SideItem';
-import Icons from '../assets/icons';
+import {
+  AccountCircleOutlined,
+  KeyboardArrowDownOutlined,
+  Home,
+  TrendingUp,
+  QueueMusic,
+  PlaylistPlay,
+  Book,
+} from '@mui/icons-material';
 
 function Sidebar(props) {
   return (
     <div id="sidebar" className={props.className}>
-      <SideItem text="Thomas" icon={Icons.profile}>
-        <img src={Icons.arrowdown} alt="Dropdown Icon" />
+      <SideItem text="Thomas" Icon={AccountCircleOutlined}>
+        <KeyboardArrowDownOutlined />
       </SideItem>
       <hr />
 
       <p className="side-heading"> MENU </p>
-      <SideItem text="Discover" icon={Icons.home} />
-      <SideItem text="Trending" icon={Icons.trending} />
-      <SideItem text="Streaming" icon={Icons.queue} />
-      <SideItem text="Playlist" icon={Icons.playlist} />
-      <SideItem text="Bookmark" icon={Icons.bookmark} />
+      <SideItem text="Discover" Icon={Home} />
+      <SideItem text="Trending" Icon={TrendingUp} />
+      <SideItem text="Streaming" Icon={QueueMusic} />
+      <SideItem text="Playlist" Icon={PlaylistPlay} />
+      <SideItem text="Bookmark" Icon={Book} />
       <hr />
 
       <p className="side-heading"> CATEGORY </p>
-      <SideItem text="Live Stream" icon={Icons.home} />
-      <SideItem text="Tutorial" icon={Icons.trending} />
-      <SideItem text="Competition" icon={Icons.queue} />
-      <SideItem text="Community" icon={Icons.playlist} />
+      <SideItem text="Live Stream" Icon={Home} />
+      <SideItem text="Tutorial" Icon={TrendingUp} />
+      <SideItem text="Competition" Icon={QueueMusic} />
+      <SideItem text="Community" Icon={PlaylistPlay} />
       <br />
     </div>
   );
